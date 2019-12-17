b23420bbdd9fcd7d7be3ee8908d140406f4d94ea
Nb <42965940+622022@users.noreply.github.com>
2019-12-17 15:04:21 +0100

    lazy

--- eventpagetest.php
+++ eventpagetest.php
@@ -26,23 +26,11 @@
     <div class="row">
         <div class="col-xl">
         <h2>TITLE HEADING</h2>
-        <h5>Title description, Dec 7, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-        <br>
-        <h2>TITLE HEADING</h2>
-        <h5>Title description, Sep 2, 2017</h5>
-        <div class="fakeimg">Fake Image</div>
-        <p>Some text..</p>
-        <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-    </div>
-        </div>
+        <button type="button" class="btn btn-primary"> Add </button>
         <div class="col"></div>
         <div class="col"></div>
     </div>
 
-    <!-- putting as is to remember -->
     <!-- <?php
     function generateProjectCard($projectName, $projectDescription, $projectLanguage)
     {
@@ -56,7 +44,7 @@
                 <img src="i/a/ico/'.$projectLanguage.'.svg" alt="'.$projectLanguage.' Logo" class="project-card-lang">
             </a>';
     }
-    ?> -->
+    ?>  -->
 
 
 
