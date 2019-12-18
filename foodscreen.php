87c17423ab1b01e63b289b0bb31cba5f9e257c94
ZachariahBoyden <42965938+ZachariahBoyden@users.noreply.github.com>
2019-12-18 08:04:24 +0100

    Add files via upload
    More event displaying.

---
+++ foodscreen.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <a href="foodtext.php">
+            <img src="download (4).jpg" alt="Click here for info about the event!">
+        </a>
+        <h2>Ratatouille!<h2>
+        <h4>Time open: 16:00 - 17:00<h4>
+        <h4>30 euros for adult, 15 euros child.<h4>
+        
+        <a href="ticketselection.php">Buy tickets here!</a>
+    </body>
+</html>
---
+++ foodtext.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <h2>Ratatouille!<h2>
+        <h4>Eat at the best French restaurant around!<h4>
+        <form action="">
+            <input type="close" name="Close">
+        </form>
+    </body>
+</html>
