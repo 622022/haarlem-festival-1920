a3d46d67ec22c9dae6b445054eb177770a892dfa
Galaxy <thomaswillyam@live.nl>
2020-01-10 22:53:16 +0100

    Designed CMS login page

--- cms/login.php
+++ cms/login.php
@@ -3,17 +3,32 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../css/cms.css" />
         <title>CMS – Login</title>
     </head>
     <body>
-        <div class="form">
-            <h1>Log In</h1>
-            <form action="controller/user-controller.php" method="post" name="login-form">
-                <input type="text" name="email" placeholder="Email" required />
-                <input type="password" name="password" placeholder="Password" required />
-                <input name="login-button" type="submit" value="Login" />
-            </form>
-            <p>Not registered yet? <a href='registration.php'>Register Here</a></p>
+        <div id="background-section">
+            <img src="../img/cms/waves.svg">
+        </div>
+        <div id="login-section">
+            <div id="login-section-controls">
+                <img src="../img/hf-logo.png" alt="Haarlem Festival Logo">
+                <h1>Haarlem Festival</h1>
+                <h2>Admin Login</h2>
+                <form action="controller/user-controller.php" method="post" name="login-form">
+                    <input type="text" name="email" placeholder="Email Address" required>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <div id="login-section-controls-rememberme">
+                        <input type="checkbox" name="login-remember">
+                        Remember me
+                    </div>
+                    <a id="login-section-controls-passreset" href="passreset.php">Forgot Password</a>
+                    <a href="register.php">
+                        <input type="button" value="Register">
+                    </a>
+                    <input type="submit" name="login-button" value="Login">
+                </form>
+            <div>
         </div>
     </body>
 </html>
