ce1254f33603bec84b4a519088389a1c83433e14
Galaxy <thomaswillyam@live.nl>
2020-01-11 18:33:40 +0100

    Finish CMS login functionality and remove index.php

--- cms/login.php
+++ cms/login.php
@@ -15,7 +15,7 @@
                 <img src="../img/hf-logo.png" alt="Haarlem Festival Logo">
                 <h1>Haarlem Festival</h1>
                 <h2>Admin Login</h2>
-                <form action="controller/user-controller.php" method="post" name="login-form">
+                <form action="../controller/user-controller.php" method="post" name="login-form">
                     <input type="text" name="email" placeholder="Email Address" required>
                     <input type="password" name="password" placeholder="Password" required>
                     <div id="login-section-controls-rememberme">
