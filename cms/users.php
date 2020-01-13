6363cabd3a0fd8c683a4fdf123d6cfbda3104a43
Galaxy <thomaswillyam@live.nl>
2020-01-13 16:59:09 +0100

    Added base of all CMS pages

---
+++ cms/users.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="../css/cms.css" />
+        <title>CMS – Events</title>
+    </head>
+    <body>
+        <?php include('./parts/sidebar.php'); ?>
+    </body>
+</html>
