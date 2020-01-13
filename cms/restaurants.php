a84f1855fb92803d191b7f84ab386875ef9475a5
Galaxy <thomaswillyam@live.nl>
2020-01-13 17:07:51 +0100

    Fix text on CMS pages

--- cms/restaurants.php
+++ cms/restaurants.php
@@ -4,7 +4,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="../css/cms.css" />
-        <title>CMS â€“ Events</title>
+        <title>CMS – Restaurants</title>
     </head>
     <body>
         <?php include('./parts/sidebar.php'); ?>
