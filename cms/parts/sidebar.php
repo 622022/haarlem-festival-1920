07f125e74dea90092186497aaefc77adbe347787
Galaxy <thomaswillyam@live.nl>
2020-01-31 23:13:08 +0100

    Update sidebar.php

--- cms/parts/sidebar.php
+++ cms/parts/sidebar.php
@@ -23,32 +23,32 @@
     <h1>Haarlem Festival</h1>
     <h2>Content Management System</h2>
     <a href="./events.php" class="sidebar-item">
-        <img src="../../img/cms/calendar.svg">
+        <img src="../img/cms/calendar.svg">
         Events
     </a>
     <a href="./users.php" class="sidebar-item">
-        <img src="../../img/cms/user.svg">
+        <img src="../img/cms/user.svg">
         Users
     </a>
     <a href="./pages.php" class="sidebar-item">
-        <img src="../../img/cms/edit.svg">
+        <img src="../img/cms/edit.svg">
         Pages
     </a>
     <a href="./tickets.php" class="sidebar-item">
-        <img src="../../img/cms/tickets.svg">
+        <img src="../img/cms/tickets.svg">
         Tickets
     </a>
     <a href="./restaurants.php" class="sidebar-item">
-        <img src="../../img/cms/restaurant.svg">
+        <img src="../img/cms/restaurant.svg">
         Restaurants
     </a>
     <a href="./invoices.php" class="sidebar-item">
-        <img src="../../img/cms/receipt.svg">
+        <img src="../img/cms/receipt.svg">
         Invoices
     </a>
     <!-- Translations footer -->
     <div id="sidebar-translations">
-        <img src="../../img/cms/translate.svg">
+        <img src="../img/cms/translate.svg">
         English
     </div>
 </div>
