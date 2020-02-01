d4608a243bdb57cca382911015d1b877d69425e5
Galaxy <thomaswillyam@live.nl>
2020-02-01 16:54:00 +0100

    Update event card generation

--- cms/events.php
+++ cms/events.php
@@ -13,7 +13,15 @@
                 include('../service/event-service.php');
                 $events = eventService::getInstance()->getAllEvents(0);
                 for ($i=0; $i < count($events); $i++) { 
-                    include('./parts/event-card.php'); 
+                    $event = $events[i];
+                    ?>
+                    <div class=\"event-card\">
+                    <h1><?= $event->getEventName() ?></h1>
+                    <h2><?= $event->artist ?></h2>
+                    <h3>January 1st – 00:00 - 00:00</h3>
+                    <button>Edit Event</button>
+                    </div>
+                    <?php
                 }
             ?>
         </div>
