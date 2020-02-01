cbd7a783e0288cac9198c8e5a5f4fdc0bf932e06
Galaxy <thomaswillyam@live.nl>
2020-02-01 18:37:27 +0100

    New translation system example

---
+++ lib/strings.php
@@ -0,0 +1,19 @@
+<?php
+    $strings["en"] = [
+        "cms.events" => "Events",
+        "cms.users" => "Users",
+        "cms.pages" => "Pages",
+        "cms.tickets" => "Tickets",
+        "cms.restaurants" => "Restaurants",
+        "cms.invoices" => "Invoices"
+    ];
+
+    $strings["nl"] = [
+        "cms.events" => "Evenementen",
+        "cms.users" => "Gebruikers",
+        "cms.pages" => "Paginas",
+        "cms.tickets" => "Tickets",
+        "cms.restaurants" => "Restaurants",
+        "cms.invoices" => "Facturen"
+    ];
+?>
