3c34a592b0730c8167de612efb6a3f1459fc0801
suicide <42965940+622022@users.noreply.github.com>
2020-03-24 16:05:13 +0100

    added Modal.

--- eventpagetest.php
+++ eventpagetest.php
@@ -101,7 +101,53 @@
       <!-- location.href just for checking the flow here -->
     </section>
 
+    
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+    <!-- Modal content -->
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <h2 id="day1" >Friday</h2>
+        <h3 id="dag1" >€125,-</h3>
+        <button id="btn1" class="addbtn" type="button" name="add">ADD</button>
+        <h2 id="day2">Saturday</h2>
+        <h3 id="dag2" >€150,-</h3>
+        <button id="btn2" class="addbtn" type="button" name="add">ADD</button>
+        <h2 id="day3">Sunday</h2>
+        <h3 id="dag3" >€150,-</h3>
+        <button id="btn3" class="addbtn" type="button" name="add">ADD</button>
+      </div>
+        <h3 id="description">You can buy the passes for one day or 3 days!</h3>
 
+    </div>
+
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("all-accessbtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+          modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+          modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+          if (event.target == modal) {
+            modal.style.display = "none";
+          }
+        }
+    </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="scripts/eventcards.js"></script>
     <script src="scripts/cart.js"></script>
