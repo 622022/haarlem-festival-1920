b17230e5dd6c318eed6a381808b87bdf99bfd977
Nb <42965940+622022@users.noreply.github.com>
2020-06-12 14:31:29 +0200

    model and credential paths changed

--- config/credentials-example.php
+++ config/credentials-example.php
@@ -8,7 +8,7 @@
     define('DB_PORT', '3308');
     */
 
-    define('DB_HOST', 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+    define('DB_HOST', 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
     define('DB_USER', 'a7a4zosbz6c8kv0m');
     define('DB_DB', 'tyyq0rwycwvceory');
     define('DB_PASSWORD', 'jttzarz0yyx4r70u');
