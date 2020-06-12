222c9886787bec418b07c16faa7db512e932a7cf
Nb <42965940+622022@users.noreply.github.com>
2020-06-12 14:24:19 +0200

    model and credential paths changed

--- config/credentials-example.php
+++ config/credentials-example.php
@@ -8,10 +8,9 @@
     define('DB_PORT', '3308');
     */
 
-    define('DB_HOST', 'hfitteam3.infhaarlem.nl');
-    define('DB_USER', 'hfitteam3_spops');
-    define('DB_DB', 'hfitteam3_haarlemfestival');
-    define('DB_PASSWORD', 'Spoceps1');
+    define('DB_HOST', 'l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	');
+    define('DB_USER', 'a7a4zosbz6c8kv0m');
+    define('DB_DB', 'tyyq0rwycwvceory');
+    define('DB_PASSWORD', 'jttzarz0yyx4r70u');
     define('DB_PORT', '3306');
 ?>
-
